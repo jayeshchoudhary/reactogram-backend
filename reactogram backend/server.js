2e1edbe6773499d1f02cfb5727541add625ca93f
Jayesh Choudhary <jayeshchoudhary619@gmail.com>
2023-01-28 14:32:10 +0530

    fix server.js

--- reactogram backend/server.js
+++ reactogram backend/server.js
@@ -2,24 +2,24 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoute = require("./routes/user_route");
 
 const { MONGOBD_URL } = require("./config");
 
 mongoose.set("strictQuery", true);
 mongoose.connect(MONGOBD_URL);
 mongoose.connection.on("connected", () => {
-  console.log("Data base connected");
+    console.log("Data base connected");
 });
 
 mongoose.connection.on("error", (error) => {
-  console.log("DataBase Not connected");
+    console.log("DataBase Not connected");
 });
 
 app.use(cors());
-app.use(express.json(), cors());
-require("./models/user_model");
-app.use(require("./routes/user_route"), cors());
+app.use(express.json());
+app.use("/", userRoute);
 
 app.listen(4000, () => {
-  console.log("Terminal started");
+    console.log("Terminal started");
 });

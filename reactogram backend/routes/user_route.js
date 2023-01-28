85f3c39454dad6ad6ce465136c6e9a310fedad2d
Jayesh Choudhary <jayeshchoudhary619@gmail.com>
2023-01-28 14:31:20 +0530

    fix user route

--- reactogram backend/routes/user_route.js
+++ reactogram backend/routes/user_route.js
@@ -1,88 +1,93 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const UserModel = mongoose.model("UserModel");
 const bcryptjs = require("bcryptjs");
+const UserModel = require("../models/user_model");
 
+// signup route
 router.post("/signup", (req, res) => {
-  const { fullName, email, password, profileImg } = req.body;
-  if (!fullName || !email || !password) {
-    return res.status(400).json({ error: "Please enter all mandotary fields" });
-  }
-  UserModel.findOne({ email: email })
-    .then((userInDB) => {
-      if (userInDB) {
+    const { fullName, email, password, profileImg } = req.body;
+    if (!fullName || !email || !password) {
         return res
-          .status(500)
-          .json({ error: "User with this email already exist" });
-      }
+            .status(400)
+            .json({ error: "Please enter all mandotary fields" });
+    }
 
-      bcryptjs
-        .hash(password, 16)
-        .then((hashedPassword) => {
-          const user = new UserModel({
-            fullName,
-            email,
-            password: hashedPassword,
-            profileImg,
-          });
-          user
-            .save()
-            .then((newUser) => {
-              res.status(201).json({ result: "User signed up successfully" });
-            })
-            .catch((err) => {
-              console.log(err);
-            });
+    UserModel.findOne({ email: email })
+        .then((userInDB) => {
+            if (userInDB) {
+                return res
+                    .status(500)
+                    .json({ error: "User with this email already exist" });
+            }
+
+            bcryptjs
+                .hash(password, 16)
+                .then((hashedPassword) => {
+                    const user = new UserModel({
+                        fullName,
+                        email,
+                        password: hashedPassword,
+                        profileImg,
+                    });
+                    user.save()
+                        .then((newUser) => {
+                            res.status(201).json({
+                                result: "User signed up successfully",
+                            });
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         })
         .catch((err) => {
-          console.log(err);
+            console.log(err);
         });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 });
-{
-  /*login back end */
-}
+
+// login backend
 router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).json({ error: "Please enter all mandotary fields" });
-  }
-  UserModel.findOne({ email: email })
-    .then((userInDB) => {
-      if (!userInDB) {
+    const { email, password } = req.body;
+    if (!email || !password) {
         return res
-          .status(401)
-          .json({ error: "Invalid Credentials" });
-      }
+            .status(400)
+            .json({ error: "Please enter all mandotary fields" });
+    }
+    UserModel.findOne({ email: email })
+        .then((userInDB) => {
+            if (!userInDB) {
+                return res.status(401).json({ error: "Invalid Credentials" });
+            }
 
-      bcryptjs
-        .hash(password, 16)
-        .then((hashedPassword) => {
-          const user = new UserModel({
-            fullName,
-            email,
-            password: hashedPassword,
-            profileImg,
-          });
-          user
-            .save()
-            .then((newUser) => {
-              res.status(201).json({ result: "User signed up successfully" });
-            })
-            .catch((err) => {
-              console.log(err);
-            });
+            bcryptjs
+                .hash(password, 16)
+                .then((hashedPassword) => {
+                    const user = new UserModel({
+                        fullName,
+                        email,
+                        password: hashedPassword,
+                        profileImg,
+                    });
+                    user.save()
+                        .then((newUser) => {
+                            res.status(201).json({
+                                result: "User signed up successfully",
+                            });
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         })
         .catch((err) => {
-          console.log(err);
+            console.log(err);
         });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 });
-module.export = router;
+
+module.exports = router;
